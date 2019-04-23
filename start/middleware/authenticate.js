c0862f50e26ddb97c26f995bff1759b1aa0419b3
gatana <sgatana@gmail.com>
2019-04-23 03:19:31 +0300

    add base url

--- start/middleware/authenticate.js
+++ start/middleware/authenticate.js
@@ -17,7 +17,7 @@ passport.use(
     {
       clientID: googleClientID,
       clientSecret: googleClietnSecret,
-      callbackURL: 'http://localhost:5000/auth/google/callback'
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
