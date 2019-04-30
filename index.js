a0fd746004dec43a9e292ac8930dcb4701bab654
gatana <sgatana@gmail.com>
2019-04-30 18:16:46 +0300

    fix conflicts

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require('./start')
 const config = require('./config/config')
 
 const PORT = config.port || 5000
-<<<<<<< HEAD
 if (process.env.NODE_ENV === 'production') {
   const express = require('express')
   const path = require('path')
@@ -11,9 +10,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-=======
-
->>>>>>> c7b8f6ad3aa2bc88627c240d77404a728a455ba9
 app.listen(PORT, error => {
   if (error) {
     console.log('An error occurred', error)
